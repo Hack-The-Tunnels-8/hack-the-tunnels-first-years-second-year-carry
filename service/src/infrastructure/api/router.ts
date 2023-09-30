60bd1bd1b53a272c7985471b82baf13fcd80a7ac
JollyCo-operation2527 <nghuyduc2527@gmail.com>
2023-09-30 13:31:13 -0400

    hello world service side

--- service/src/infrastructure/api/router.ts
+++ service/src/infrastructure/api/router.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const getAPIRoot = async (_: Request, response: Response) => {
   response.json({
-    message: "Hellow world",
+    message: "Hello world",
   });
 };
 
